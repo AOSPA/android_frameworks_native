a8714da38d820a97e876fbc61c9005cf61a00930
Jake Weinstein <xboxlover360@gmail.com>
2016-10-06 01:19:19 -0400

    surfaceflinger: fix compilation for non-HWC2 targets
    * Thank you to Altaf-Mahdi and Steve Kondik for the original fixes.
      This is a combines parts of their commits.
    
    Change-Id: I31e733f6fab6cd87913d837b476ead1c02e42fc9

--- services/surfaceflinger/DisplayUtils.cpp
+++ services/surfaceflinger/DisplayUtils.cpp
@@ -100,9 +100,11 @@ HWComposer* DisplayUtils::getHWCInstance(
     if(sUseExtendedImpls) {
         return new ExHWComposer(flinger, handler);
     }
-#endif
+    return new HWComposer(flinger, handler);
+#else
     (void)handler;
     return new HWComposer(flinger);
+#endif
 }
 
 void DisplayUtils::initVDSInstance(HWComposer* hwc, int32_t hwcDisplayId,
