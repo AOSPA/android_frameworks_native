d4f790b080bcb8666ff730c0cbc7dadc84546ba2
Alex Naidis <alex.naidis@linux.com>
2016-12-01 20:15:51 +0100

    binder: enable SDCLANG LTO
    Enabling SDCLANG LTO for libbinder
    since it is a critical part for
    system performance.
    
    Change-Id: Ib36d1c923c0b623e8f59aaf7e4287e9e5a40efbd
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>

--- libs/binder/Android.mk
+++ libs/binder/Android.mk
@@ -49,6 +49,7 @@ LOCAL_MODULE := libbinder
 LOCAL_SHARED_LIBRARIES := liblog libcutils libutils
 
 LOCAL_CLANG := true
+LOCAL_SDCLANG_LTO := true
 LOCAL_SANITIZE := integer
 LOCAL_SRC_FILES := $(sources)
 ifneq ($(TARGET_USES_64_BIT_BINDER),true)
@@ -62,6 +63,7 @@ include $(BUILD_SHARED_LIBRARY)
 include $(CLEAR_VARS)
 LOCAL_MODULE := libbinder
 LOCAL_STATIC_LIBRARIES += libutils
+LOCAL_SDCLANG_LTO := true
 LOCAL_SRC_FILES := $(sources)
 ifneq ($(TARGET_USES_64_BIT_BINDER),true)
 ifneq ($(TARGET_IS_64_BIT),true)
