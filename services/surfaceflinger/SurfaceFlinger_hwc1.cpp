af17cc9f20c72e50743025f96da32596d80dd8a9
Alex Naidis <alex.naidis@linux.com>
2016-12-20 23:44:23 +0100

    SF: Adjust RT sched priorities
    2->4
    1->2
    
    This converts the sched prios
    of RT processes to be able to schedule
    other RT process lower or in between
    the short- and long running SF processes.
    
    This change by itself is no functional
    change.
    
    Change-Id: I24fe2d60440f32369d3c43bab4efcb5c2c02652b
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>

--- services/surfaceflinger/SurfaceFlinger_hwc1.cpp
+++ services/surfaceflinger/SurfaceFlinger_hwc1.cpp
@@ -476,7 +476,7 @@ void SurfaceFlinger::init() {
 		
        // set SFEventThread to SCHED_FIFO to minimize jitter
        struct sched_param param = {0};
-       param.sched_priority = 2;
+       param.sched_priority = 4;
        if (sched_setscheduler(mSFEventThread->getTid(), SCHED_FIFO, &param) != 0) {
            ALOGE("Couldn't set SCHED_FIFO for SFEventThread");
        }
@@ -488,7 +488,7 @@ void SurfaceFlinger::init() {
 		
        // set EventThread to SCHED_FIFO to minimize jitter
        struct sched_param param = {0};
-       param.sched_priority = 2;
+       param.sched_priority = 4;
        if (sched_setscheduler(mEventThread->getTid(), SCHED_FIFO, &param) != 0) {
            ALOGE("Couldn't set SCHED_FIFO for SFEventThread");
        }
@@ -2666,7 +2666,7 @@ void SurfaceFlinger::setPowerModeInternal(const sp<DisplayDevice>& hw,
         repaintEverything();
 
         struct sched_param param = {0};
-        param.sched_priority = 1;
+        param.sched_priority = 2;
         if (sched_setscheduler(0, SCHED_FIFO, &param) != 0) {
             ALOGW("Couldn't set SCHED_FIFO on display on");
         }
