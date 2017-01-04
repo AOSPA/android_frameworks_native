af46fe75e80510438f46ef2a84eb36074441a7d0
Alex Naidis <alex.naidis@linux.com>
2017-01-04 22:33:44 +0100

    SF: Enable SDCLANG LTO
    Libsurfaceflinger consists of many source files
    and is critical for performance.
    
    Enabling SDCLANG LTO has a benefit here.
    
    Change-Id: If52874e838985d76c923e611c47b748585b95dcf
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>

--- services/surfaceflinger/Android.mk
+++ services/surfaceflinger/Android.mk
@@ -171,6 +171,8 @@ LOCAL_MODULE := libsurfaceflinger
 
 LOCAL_CFLAGS += -Wall -Werror -Wunused -Wunreachable-code
 
+LOCAL_SDCLANG_LTO := true
+
 include $(BUILD_SHARED_LIBRARY)
 
 ###############################################################
