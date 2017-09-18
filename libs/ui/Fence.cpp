e1c48d94955207585837ec1d990c8ba45bf3d6de
Prabhanjan Kandula <pkandula@codeaurora.org>
2017-09-18 16:25:56 -0700

    libui : Dump sync fence on timeout
    On sync wait failure dump all the underlying sync points
    attached to the fence and the stack frame of the caller.
    
    Change-Id: Ib4f443ade3eb55bc7012a14f16899fd04f2e06b0

--- libs/ui/Fence.cpp
+++ libs/ui/Fence.cpp
@@ -32,6 +32,7 @@
 #include <utils/Log.h>
 #include <utils/String8.h>
 #include <utils/Trace.h>
+#include <utils/CallStack.h>
 
 namespace android {
 
@@ -57,6 +58,10 @@ status_t Fence::wait(int timeout) {
         return NO_ERROR;
     }
     int err = sync_wait(mFenceFd, timeout);
+    if (err < 0 && (timeout == TIMEOUT_NEVER || timeout >100)) {
+        ALOGE("ERROR :Fence didnt signal in %dms. Initiating dump", timeout);
+        dump();
+    }
     return err < 0 ? -errno : status_t(NO_ERROR);
 }
 
@@ -68,11 +73,33 @@ status_t Fence::waitForever(const char* logname) {
     int warningTimeout = 3000;
     int err = sync_wait(mFenceFd, warningTimeout);
     if (err < 0 && errno == ETIME) {
-        ALOGE("%s: fence %d didn't signal in %u ms", logname, mFenceFd,
+        ALOGE("%s: fence %d didn't signal in %ums. --Initiating dump", logname, mFenceFd,
                 warningTimeout);
+        dump();
         err = sync_wait(mFenceFd, TIMEOUT_NEVER);
     }
     return err < 0 ? -errno : status_t(NO_ERROR);
+}
+
+status_t Fence::dump() const {
+    if (mFenceFd == -1) {
+        return NO_ERROR;
+    }
+
+    // Print callstack to understand caller
+    CallStack stack("FENCE_DUMP");
+
+    struct sync_fence_info_data* finfo = sync_fence_info(mFenceFd);
+    struct sync_pt_info* pinfo = NULL;
+
+    ALOGE(" ----- Printing sync-points under fence fd:%d status:%d name:%s -----",
+            mFenceFd, finfo->status, finfo->name);
+    while ((pinfo = sync_pt_info(finfo, pinfo)) != NULL) {
+        ALOGE("status:%d driver:%s obj:%s", pinfo->status,
+                pinfo->driver_name,  pinfo->obj_name);
+    }
+
+    return NO_ERROR;
 }
 
 sp<Fence> Fence::merge(const char* name, const sp<Fence>& f1,
