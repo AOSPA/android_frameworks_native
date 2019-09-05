beeb13eb69b70cc956b3342980260f7719e95bd1
Arthur Hung <arthurhung@google.com>
2019-09-05 16:38:18 +0800

    Revert "Fix drag and drop (2/3)"
    This reverts commit fbe5d9c4233dcd802a122f70ca5a3641fcd556ca.
    
    Bug: 137819199
    Test: manual
    Change-Id: I7afec569519b9c69eb03225672db6db141b20241
    Merged-In: I7afec569519b9c69eb03225672db6db141b20241
    (cherry picked from commit 6cbb97591535868d629a562dfd7d8e24865cf551)

--- services/surfaceflinger/SurfaceFlinger.cpp
+++ services/surfaceflinger/SurfaceFlinger.cpp
@@ -3703,7 +3703,7 @@ void SurfaceFlinger::updateInputFlinger() {
         setInputWindowsFinished();
     }
 
-    executeInputWindowCommands();
+    mInputWindowCommands.clear();
 }
 
 void SurfaceFlinger::updateInputWindowInfo() {
@@ -3725,19 +3725,6 @@ void SurfaceFlinger::updateInputWindowInfo() {
 void SurfaceFlinger::commitInputWindowCommands() {
     mInputWindowCommands = mPendingInputWindowCommands;
     mPendingInputWindowCommands.clear();
-}
-
-void SurfaceFlinger::executeInputWindowCommands() {
-    for (const auto& transferTouchFocusCommand : mInputWindowCommands.transferTouchFocusCommands) {
-        if (transferTouchFocusCommand.fromToken != nullptr &&
-            transferTouchFocusCommand.toToken != nullptr &&
-            transferTouchFocusCommand.fromToken != transferTouchFocusCommand.toToken) {
-            mInputFlinger->transferTouchFocus(transferTouchFocusCommand.fromToken,
-                                              transferTouchFocusCommand.toToken);
-        }
-    }
-
-    mInputWindowCommands.clear();
 }
 
 void SurfaceFlinger::updateCursorAsync()
