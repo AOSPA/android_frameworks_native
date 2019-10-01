33ba5abc9c5a8f658b2aa360a1473609b3324799
Naveen Kumar <kumarn@codeaurora.org>
2019-10-01 14:58:17 +0530

    sf: Send cutout hint to hwc.
    HWC uses cutout hint to optimize composition.
    CRs-Fixed: 2526089
    
    Change-Id: Id2a4758e5046e373bdbf242454927634679ccfa8

--- services/surfaceflinger/SurfaceFlinger.cpp
+++ services/surfaceflinger/SurfaceFlinger.cpp
@@ -2054,10 +2054,10 @@ void SurfaceFlinger::setDisplayAnimating(const sp<DisplayDevice>& hw) {
     hw->setAnimating(hasScreenshot);
 }
 
-void SurfaceFlinger::setLayerAsMask(const sp<DisplayDevice>& hw, const uint64_t& layerId) {
-    using vendor::display::config::V1_7::IDisplayConfig;
-    android::sp<IDisplayConfig> disp_config_v1_7 = IDisplayConfig::getService();
 
+void SurfaceFlinger::setLayerAsMask(const sp<const DisplayDevice>& hw, const uint64_t& layerId) {
+    static android::sp<vendor::display::config::V1_7::IDisplayConfig> disp_config_v1_7 =
+                                        vendor::display::config::V1_7::IDisplayConfig::getService();
     const std::optional<DisplayId>& displayId = hw->getId();
     const auto dpy = getHwComposer().fromPhysicalDisplayId(*displayId);
     if (!disp_config_v1_7) {
@@ -2148,9 +2148,6 @@ void SurfaceFlinger::calculateWorkingSet() {
             }
 
             const auto& displayState = display->getState();
-            if (layer->getPrimaryDisplayOnly() && layer->hasHwcLayer(displayDevice)) {
-                setLayerAsMask(displayDevice, (layer->getHwcLayer(displayDevice))->getId());
-            }
             layer->setPerFrameData(displayDevice, displayState.transform, displayState.viewport,
                                    displayDevice->getSupportedPerFrameMetadata(),
                                    isHdrColorMode(displayState.colorMode) ? Dataspace::UNKNOWN
