AOSPA/android_frameworks_native

commit 91f0772922d66f2a063271c883e86e914c75388f

sf: Get latest vsync period from HWC
 "getVsyncPeriodFromHWC()" returns latest fps. No need for any caching.

Change-Id: I08724a4767c84db8006c46576cb536d99cbe99cd
CRs-Fixed: 3295221

services/surfaceflinger/SurfaceFlinger.cpp
@@ -3664,8 +3664,7 @@ void SurfaceFlinger::updateVsyncSource()
         mScheduler->onScreenReleased(mAppConnectionHandle);
     } else if (mNextVsyncSource && (mActiveVsyncSource == NULL)) {
         mScheduler->onScreenAcquired(mAppConnectionHandle);
-        bool isPrimary = mNextVsyncSource->isPrimary();
-        nsecs_t vsync = (isPrimary && (mVsyncPeriod > 0)) ? mVsyncPeriod : getVsyncPeriodFromHWC();
+        nsecs_t vsync = getVsyncPeriodFromHWC();
         mScheduler->resyncToHardwareVsync(true, Fps::fromPeriodNsecs(vsync));
     } else if ((mNextVsyncSource != NULL) &&
         (mActiveVsyncSource != NULL)) {

commit 68fd15f481f4bc04b35203370c25ce2bdb3254a8

SF: Make "Get latest vsync period from HWC" optional
Devices with older display HAL seem to return incorrect vsync period
after wakeup, which caused massive lags after 91f07729. Allow devices
to revert back to the older behaviour by setting
ro.sf.use_latest_hwc_vsync_period=0 in system prop.

Change-Id: Iba81f6981abd514ce5887fba6320480f2d906185

services/surfaceflinger/SurfaceFlinger.cpp
@@ -641,6 +641,9 @@ SurfaceFlinger::SurfaceFlinger(Factory& factory) : SurfaceFlinger(factory, SkipI
     property_get("ro.sf.force_hwc_brightness", value, "0");
     mForceHwcBrightness = atoi(value);
 
+    property_get("ro.sf.use_latest_hwc_vsync_period", value, "1");
+    mUseLatestHwcVsyncPeriod = atoi(value);
+
     char property[PROPERTY_VALUE_MAX] = {0};
     if((property_get("vendor.display.vsync_reliable_on_doze", property, "0") > 0) &&
         (!strncmp(property, "1", PROPERTY_VALUE_MAX ) ||
@@ -3695,7 +3698,9 @@ void SurfaceFlinger::updateVsyncSource()
         mScheduler->onScreenReleased(mAppConnectionHandle);
     } else if (mNextVsyncSource && (mActiveVsyncSource == NULL)) {
         mScheduler->onScreenAcquired(mAppConnectionHandle);
-        nsecs_t vsync = getVsyncPeriodFromHWC();
+        bool isPrimary = mNextVsyncSource->isPrimary();
+        nsecs_t vsync = (!mUseLatestHwcVsyncPeriod && isPrimary && (mVsyncPeriod > 0))
+                ? mVsyncPeriod : getVsyncPeriodFromHWC();
         mScheduler->resyncToHardwareVsync(true, Fps::fromPeriodNsecs(vsync));
     } else if ((mNextVsyncSource != NULL) &&
         (mActiveVsyncSource != NULL)) {

services/surfaceflinger/SurfaceFlinger.h
@@ -1555,6 +1555,7 @@ private:
     bool mLumaSampling = true;
     bool mForceLightBrightness = false;
     bool mForceHwcBrightness = false;
+    bool mUseLatestHwcVsyncPeriod = true;
     sp<RegionSamplingThread> mRegionSamplingThread;
     sp<FpsReporter> mFpsReporter;
     sp<TunnelModeEnabledReporter> mTunnelModeEnabledReporter;