a0181b26f0a9fc18d1de571802d50ce3e4221d7f
Rheygine Medel <quic_rmedel@quicinc.com>
2023-01-04 16:18:32 -0800

    sf: Add support for setDisplayElapseTime
    Change-Id: I0b30124650a88497ff699300773f7ad7cbaa3cfe
    CRs-Fixed: 3365272

--- services/surfaceflinger/QtiExtension/QtiHWComposerExtension.cpp
+++ services/surfaceflinger/QtiExtension/QtiHWComposerExtension.cpp
@@ -4,11 +4,21 @@
 
 // #define LOG_NDEBUG 0
 #include "QtiHWComposerExtension.h"
+#include "QtiComposerHalExtension.h"
+
+#define LOG_DISPLAY_ERROR(displayId, msg) \
+    ALOGE("%s failed for display %s: %s", __FUNCTION__, to_string(displayId).c_str(), msg)
 
 namespace android::surfaceflingerextension {
 
 QtiHWComposerExtension::QtiHWComposerExtension(android::impl::HWComposer& hwc)
       : mQtiHWComposer(hwc) {}
+
+QtiHWComposerExtension::QtiHWComposerExtension(android::impl::HWComposer& hwc,
+                                               Hwc2::Composer* composerHal)
+      : mQtiHWComposer(hwc) {
+    mQtiComposerHalExtn = new QtiComposerHalExtension(composerHal);
+}
 
 std::optional<hal::HWDisplayId> QtiHWComposerExtension::qtiFromVirtualDisplayId(
         HalVirtualDisplayId displayId) const {
@@ -18,6 +28,28 @@ std::optional<hal::HWDisplayId> QtiHWComposerExtension::qtiFromVirtualDisplayId(
         return it->second.hwcDisplay->getId();
     }
     return {};
+}
+
+status_t QtiHWComposerExtension::qtiSetDisplayElapseTime(HalDisplayId displayId,
+                                                         uint64_t timeStamp) {
+    if (mQtiHWComposer.mDisplayData.empty()) {
+        ALOGV("HWComposer's displayData is empty");
+        return BAD_VALUE;
+    }
+
+    if (mQtiHWComposer.mDisplayData.count(displayId) == 0) {
+        LOG_DISPLAY_ERROR(displayId, "Invalid display");
+        return UNKNOWN_ERROR;
+    }
+
+    const auto& displayData = mQtiHWComposer.mDisplayData[displayId];
+    auto halHWDisplayId = displayData.hwcDisplay->getId();
+    auto error = mQtiComposerHalExtn->qtiSetDisplayElapseTime(halHWDisplayId, timeStamp);
+    if (error != hal::Error::NONE) {
+        return BAD_VALUE;
+    }
+
+    return NO_ERROR;
 }
 
 } // namespace android::surfaceflingerextension
---
+++ services/surfaceflinger/QtiExtension/QtiHidlComposerHalExtension.cpp
@@ -0,0 +1,25 @@
+/* Copyright (c) 2023 Qualcomm Innovation Center, Inc. All rights reserved.
+ * SPDX-License-Identifier: BSD-3-Clause-Clear
+ */
+#define LOG_NDEBUG 0
+#include "QtiHidlComposerHalExtension.h"
+
+namespace android::surfaceflingerextension {
+
+QtiHidlComposerHalExtension::QtiHidlComposerHalExtension(Hwc2::HidlComposer* composerHal) {
+    if (!composerHal) {
+        ALOGW("Passed an invalid pointer to composer hal");
+        return;
+    }
+
+    mQtiHidlComposer = composerHal;
+    ALOGV("Successfully created QtiHidlComposerHalExtension %p", mQtiHidlComposer);
+}
+
+Error QtiHidlComposerHalExtension::qtiSetDisplayElapseTime(Display display, uint64_t timeStamp) {
+    mQtiHidlComposer->mWriter.selectDisplay(display);
+    mQtiHidlComposer->mWriter.qtiSetDisplayElapseTime(timeStamp);
+    return Error::NONE;
+}
+
+} // namespace android::surfaceflingerextension
