2b167b9003b9f3f5c2e6830aca69152f57dcb961
Naseer Ahmed <quic_naseer@quicinc.com>
2023-03-20 21:46:35 -0400

    surfaceflinger: extensions: Fix for thread safety
    Change-Id: I6c28dee9467ea7d11c49134856b7aba7595261cd
    CRs-Fixed: 3437455

--- services/surfaceflinger/QtiExtension/QtiNullExtension.cpp
+++ services/surfaceflinger/QtiExtension/QtiNullExtension.cpp
@@ -4,6 +4,8 @@
 #include "QtiNullExtension.h"
 #include <ui/DisplayId.h>
 
+#include "MutexUtils.h"
+
 namespace android::surfaceflingerextension {
 
 QtiNullExtension::QtiNullExtension() {
@@ -112,7 +114,8 @@ status_t QtiNullExtension::qtiBinderSetWideModePreference(uint64_t displayId, in
 android::VirtualDisplayId QtiNullExtension::qtiAcquireVirtualDisplay(ui::Size resolution,
                                                                      ui::PixelFormat format,
                                                                      bool canAllocateHwcForVDS) {
-    // Fix thread safety analysis
+    ConditionalLock lock(mQtiFlinger->mStateLock,
+                         std::this_thread::get_id() != mQtiFlinger->mMainThreadId);
     return mQtiFlinger->acquireVirtualDisplay(resolution, format);
 }
 bool QtiNullExtension::qtiCanAllocateHwcDisplayIdForVDS(const DisplayDeviceState& state) {
