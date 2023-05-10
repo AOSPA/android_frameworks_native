ce89abba430c91602714a41ee586f6d1f99776a6
Rheygine Medel <quic_rmedel@quicinc.com>
2023-05-09 17:58:47 -0700

    sf: extensions: Add support for fb scaling
    This change propagates the ff:
    sf: add support to change the size of FrameBuffer target
    // Change-Id: I0ce98febcaff154f1edd791550a68486e68af8ba
    
    sf: perform FB scaling purely based on LayerStackRect parameters.
    // Change-Id: I4e9aed8c4ccc7a80ebc2dfe931eddbc06f64daa6
    
    sf: Handle wm size changes during power off
    // Change-Id: I7e525198129b383f2b71c3adbfbcbfe29e19dc71
    
    Change-Id: Ic011b30a5c5bf2bf859818a5d9be8c3dac4d2be8
    CRs-Fixed: 2526524

--- libs/renderengine/include/renderengine/RenderEngine.h
+++ libs/renderengine/include/renderengine/RenderEngine.h
@@ -203,6 +203,10 @@ public:
 
     virtual void setEnableTracing(bool /*tracingEnabled*/) {}
 
+    /* QTI_BEGIN */
+    virtual void setViewportAndProjection(Rect viewPort, Rect sourceCrop) = 0;
+    /* QTI_END */
+
 protected:
     RenderEngine() : RenderEngine(RenderEngineType::GLES) {}
 
--- services/surfaceflinger/QtiExtension/QtiSurfaceFlingerExtension.h
+++ services/surfaceflinger/QtiExtension/QtiSurfaceFlingerExtension.h
@@ -240,6 +240,13 @@ public:
     void qtiHandleNewLevelFps(float currFps, float newLevelFps, float* fpsToSet);
     void qtiNotifyResolutionSwitch(int displayId, int32_t width, int32_t height,
                                    int32_t vsyncPeriod) override;
+    void qtiSetFrameBufferSizeForScaling(sp<DisplayDevice> displayDevice,
+                                         DisplayDeviceState& currentState,
+                                         const DisplayDeviceState& drawingState) override;
+    void qtiFbScalingOnBoot() override;
+    bool qtiFbScalingOnDisplayChange(const wp<IBinder>& displayToken, sp<DisplayDevice> display,
+                                     const DisplayDeviceState& drawingState) override;
+    void qtiFbScalingOnPowerChange(sp<DisplayDevice> display) override;
 
 private:
     SmomoIntf* qtiGetSmomoInstance(const uint32_t layerStackId) const;
@@ -265,6 +272,7 @@ private:
     bool mQtiSFExtnBootComplete = false;
     bool mQtiTidSentSuccessfully = false;
     bool mQtiWakeUpPresentationDisplays = false;
+    bool mQtiDisplaySizeChanged = false;
     int mQtiFirstApiLevel = 0;
     int mQtiRETid = 0;
     int mQtiSFTid = 0;
