685a76bf52e1a130c7c36e8f7e4f6a90b81a742e
Adithya R <gh0strider.2k18.reborn@gmail.com>
2023-09-02 22:15:44 +0530

    SF: Defer setDesiredActiveMode when display is off
    This is required on some devices with video mode panel, since the
    scheduler sets refresh rate to 120 Hz _after_ turning screen off at
    60 Hz (VRR idle) but the HWC ignores this and when the display is
    turned back on SF thinks we are back at 120 Hz while HWC is still
    rendering at 60 Hz. Defer setting refresh rate until the screen is
    turned back on, to avoid this confusion.
    
    To enable, set: ro.sf.defer_refresh_rate_when_off=1
    
    Change-Id: I3e531e601e9cb015b3066ae98d4d9c5436b46619

--- services/surfaceflinger/SurfaceFlinger.h
+++ services/surfaceflinger/SurfaceFlinger.h
@@ -1370,6 +1370,10 @@ private:
     scheduler::PresentLatencyTracker mPresentLatencyTracker GUARDED_BY(kMainThreadContext);
 
     bool mLumaSampling = true;
+
+    bool mDeferRefreshRateWhenOff = false;
+    std::optional<scheduler::FrameRateMode> mLastActiveMode GUARDED_BY(mStateLock);
+
     sp<RegionSamplingThread> mRegionSamplingThread;
     sp<FpsReporter> mFpsReporter;
     sp<TunnelModeEnabledReporter> mTunnelModeEnabledReporter;
