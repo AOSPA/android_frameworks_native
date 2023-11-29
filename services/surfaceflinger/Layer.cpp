1f6bd698bab078a7524ae51b049ab3fa2a0595b0
Alec Mouri <alecmouri@google.com>
2023-11-29 16:42:03 +0000

    Assign previous release fence to previous frame ID
    ...as otherwise the release timestamps for EGL/Vk timing apis are
    shifted by one frame.
    
    Bug: 295456126
    Bug: 301055892
    Bug: 303385401
    Test: Log and inspect timestamps
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:a7e332e0e31b17388c1d15f5046f98f1cc3b51b9)
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:cd21dd8729629a0293189c529dd891f8bee86cf7)
    Merged-In: If1d8a525053591f15b95aeec8dc0bad8a13e6833
    Change-Id: If1d8a525053591f15b95aeec8dc0bad8a13e6833

--- services/surfaceflinger/Layer.cpp
+++ services/surfaceflinger/Layer.cpp
@@ -187,6 +187,7 @@ Layer::Layer(const LayerCreationArgs& args)
     mDrawingState.sequence = 0;
     mDrawingState.transform.set(0, 0);
     mDrawingState.frameNumber = 0;
+    mDrawingState.previousFrameNumber = 0;
     mDrawingState.barrierFrameNumber = 0;
     mDrawingState.producerId = 0;
     mDrawingState.barrierProducerId = 0;
@@ -2922,6 +2923,10 @@ void Layer::onLayerDisplayed(ftl::SharedFuture<FenceResult> futureFenceResult,
         ch->name = mName;
     }
     mPreviouslyPresentedLayerStacks.push_back(layerStack);
+
+    if (mDrawingState.frameNumber > 0) {
+        mDrawingState.previousFrameNumber = mDrawingState.frameNumber;
+    }
 }
 
 void Layer::onSurfaceFrameCreated(
@@ -3100,6 +3105,7 @@ bool Layer::setPosition(float x, float y) {
 void Layer::resetDrawingStateBufferInfo() {
     mDrawingState.producerId = 0;
     mDrawingState.frameNumber = 0;
+    mDrawingState.previousFrameNumber = 0;
     mDrawingState.releaseBufferListener = nullptr;
     mDrawingState.buffer = nullptr;
     mDrawingState.acquireFence = sp<Fence>::make(-1);
@@ -3394,6 +3400,7 @@ bool Layer::setTransactionCompletedListeners(const std::vector<sp<CallbackHandle
             // If this transaction set an acquire fence on this layer, set its acquire time
             handle->acquireTimeOrFence = mCallbackHandleAcquireTimeOrFence;
             handle->frameNumber = mDrawingState.frameNumber;
+            handle->previousFrameNumber = mDrawingState.previousFrameNumber;
 
             // Store so latched time and release fence can be set
             mDrawingState.callbackHandles.push_back(handle);
