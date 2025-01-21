3ce8d0345f6227d9babe8da66170f6724b59c026
Dhruv Kaushik <quic_dhrukaus@quicinc.com>
2025-01-21 17:09:29 +0530

    sf: Add null check for snapshot
    Change-Id: Ia1c30c91460d3201d1ac2ace568e3a60119923f6
    CRs-Fixed: 4030886

--- services/surfaceflinger/SurfaceFlinger.cpp
+++ services/surfaceflinger/SurfaceFlinger.cpp
@@ -2668,7 +2668,9 @@ bool SurfaceFlinger::updateLayerSnapshots(VsyncId vsyncId, nsecs_t frameTimeNs,
         mLayersWithQueuedFrames.emplace(it->second, gameMode);
 
         /* QTI_BEGIN */
-        snapshot->qtiLayerClass = it->second->qtiGetLayerClass();
+        if (snapshot) {
+            snapshot->qtiLayerClass = it->second->qtiGetLayerClass();
+        }
         /* QTI_END */
     }
 
