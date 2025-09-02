15a40b801e16db882c34758b2834cab9675cf02d
Dheepthi S <quic_dheepthi@quicinc.com>
2025-09-02 15:09:10 +0530

    renderengine: Add clamp in Lutshader
    Change-Id: I43e345d493f14e5c246cec673125dbdba7a91680
    CRs-Fixed: 4277435

--- libs/renderengine/skia/filters/LutShader.cpp
+++ libs/renderengine/skia/filters/LutShader.cpp
@@ -57,6 +57,7 @@ static const SkString kShader = SkString(R"(
         /* QTI_BEGIN */
         if (lutSourceIsHwc == 1) {
           linear = rgba.rgb;
+          linear = clamp(linear,float3(0.0),float3(1.0));
         }
         /* QTI_END */
         if (dimension == 1) {
